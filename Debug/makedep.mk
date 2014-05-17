f7cc4433c59be86eb05334a4e169ed12cbc90d8d
Anthony <dragonslayer.anthony.meyer@gmail.com>
2014-05-17 19:20:21 -0400

    7:20PM

---
+++ Debug/makedep.mk
@@ -0,0 +1,14 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+BattleShip.cpp
+
+game.cpp
+
+screen.cpp
+
+shipset.cpp
+
+wegotthefunk.cpp
+
--- shipset.cpp
+++ shipset.cpp
@@ -38,46 +38,50 @@ void setDingy( screen &current_screen ) {
 
 void setDualDingy( screen &current_screen ) {
 	unsigned char input_direction;					//var used to move ship on board
-	unsigned char x_pos, y_pos;						//coordinate variables
-	x_pos = 0;
-	y_pos = 0;
-	current_screen.addLight( x_pos, y_pos );		//lights up coordinate ( 1, 1 )
-	current_screen.addLight( x_pos, y_pos + 1 );	//lights up coordinate ( 1, 2 )
-	current_screen.addLight( x_pos, y_pos + 2 );	//lights up coordinate ( 1, 3 )
+	unsigned char row, col;						//coordinate variables
+	row = 0;
+	col = 0;
+	current_screen.addLight( row, col );		//lights up coordinate ( 0, 0 )
+	current_screen.addLight( row + 1, col );	//lights up coordinate ( 1, 0 )
+	
 	
 	while ( 1 ) {
 		input_direction = getInput( current_screen );
-		if ( input_direction == 0 && y_pos != 0 ) {
-			current_screen.subLight( x_pos, y_pos );
-			current_screen.subLight( x_pos + 1, y_pos );
-			current_screen.subLight( x_pos + 2, y_pos );
-			y_pos--;
-			current_screen.subLight( x_pos, y_pos );
-			current_screen.subLight( x_pos + 1, y_pos );
-			current_screen.subLight( x_pos + 2, y_pos );
+		if ( input_direction == 0 && row != 0 ) {
+			current_screen.subLight( row + 1, col );
+			row--;
+			current_screen.addLight( row, col );
 		}
-		else if ( input_direction == 1 && y_pos != 3 ) {
-			current_screen.subLight( x_pos, y_pos );
-			current_screen.subLight( x_pos + 1, y_pos );
-			current_screen.subLight( x_pos + 2, y_pos );
-			y_pos++;
-			current_screen.subLight( x_pos, y_pos );
-			current_screen.subLight( x_pos + 1, y_pos );
-			current_screen.subLight( x_pos + 2, y_pos );
+		else if ( input_direction == 1 && row != 2 ) {
+			current_screen.subLight( row, col );
+			row++;
+			current_screen.addLight( row + 1, col );
 		}
-		else if ( input_direction == 2 && x_pos != 0 ) {
-			current_screen.subLight( x_pos + 2, y_pos );
-			x_pos--;
-			current_screen.addLight( x_pos , y_pos );
+		else if ( input_direction == 2 && col != 0 ) {
+			current_screen.subLight( row, col );
+			current_screen.subLight( row + 1, col );
+			col--;
+			current_screen.addLight( row, col );
+			current_screen.addLight( row + 1, col );
 		}
-		else if ( input_direction == 3 && x_pos != 1 ) {
-			current_screen.subLight( x_pos, y_pos );
-			x_pos++;
-			current_screen.addLight( x_pos + 2, y_pos );
+		else if ( input_direction == 3 && col != 3 ) {
+			current_screen.subLight( row, col );
+			current_screen.subLight( row + 1, col );
+			col++;
+			current_screen.addLight( row, col );
+			current_screen.addLight( row + 1, col );
 		}
 		else if ( input_direction == 4 ) {
 			if ( current_screen.collisionCheck( 3 ) == false ) {
 				break;
+			}
+			else {
+				current_screen.clearGrid();
+				setDingy( current_screen );
+				row = 0;
+				col = 0;
+				current_screen.addLight( row, col );		//lights up coordinate ( 0, 0 )
+				current_screen.addLight( row + 1, col );	
 			}
 		}
 		current_screen.flicker();
