abbaa038e0d17be1dea0c4de3f03b0dbd3bb7a38
goonsworth <hjcccstuff@gmail.com>
2014-05-17 23:43:38 -0400

    Added buttonDelay

--- wegotthefunk.cpp
+++ wegotthefunk.cpp
@@ -3,28 +3,44 @@
 #include "wegotthefunk.h"
 #define F_CPU 16000000UL
 #include <util/delay.h>
+#include <time.h>
 
+using namespace std;
 
+bool buttonDelay(float delay, bool buttonPress){
+  clock_t clockTime;
+  if( buttonPress == false)
+    clockTime = clock();
+  
+  if ( ((float)clockTime/CLOCKS_PER_SEC) >= delay )
+    return true;
+  
+  return false;
+}
 
 char getInput( screen current_screen ) {
 	char i = ( 0 );
-	while ( true ) {
-		if ( ( PIND & ( 1 << ( i + 4 ) ) ) == ( 1 << ( i + 4 ) ) ) {
-			_delay_ms( 500 );
-			return ( i );
-		}
-		if ( ( PIND & 0x08 ) == 0x08 ) {
-			_delay_ms( 500 );
-			return ( 4 );
-		}
-		else {
-			current_screen.flicker();
-			i++;
-			if ( i == 4 ) {
-				i = 0;
-			}
-		}
-	}
+	bool buttonPress = false;
+  float delay = .5;
+  
+  while ( true ) {
+    if ( ( PIND & ( 1 << ( i + 4 ) ) ) == ( 1 << ( i + 4 ) )){
+      if ( buttonDelay(delay, buttonPress) )
+        return i;
+      buttonPress = true;
+    }
+    if ( ( PIND & 0x08 ) == 0x08 ) {
+      if ( buttonDelay(delay, buttonPress) )
+        return 4;
+      buttonPress = true;
+    }
+    else {
+      current_screen.flicker();
+      i++;
+      if ( i == 4 )
+        i = 0;
+    }
+  }
 }
 
 void clearScreen( void ) {
--- wegotthefunk.h
+++ wegotthefunk.h
@@ -1,6 +1,7 @@
 #ifndef WEGOTTHEFUNK_H
 #define WEGOTTHEFUNK_H
 
+bool buttonDelay(float delay, bool buttonPress);
 char getInput( screen current_screen );
 void clearScreen( void );
 
